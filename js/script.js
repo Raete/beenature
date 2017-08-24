1aa749c56635bd765b40d6a88d5e3bcfefc0c4ea
reate <kattovera00@gmail.com>
2017-08-24 20:26:13 +0200

    add javascript

---
+++ js/script.js
@@ -0,0 +1,29 @@
+(function($) {
+
+
+//scrolování v hlavním menu
+  var menu = $('.headline__wrapper'),
+      menuLinks = menu.find('a');
+
+  menuLinks.on('click', function(event){
+      event.preventDefault();
+    
+    var id = this.hash;
+
+    $('html,body').animate({ scrollTop: $(id).offset().top }, function() {
+      window.location.hash = id;
+    });
+  });
+
+
+$( "#green-article" ).click(function(e) {
+  e.preventDefault();
+  $(".full-article").addClass('active');
+});
+
+$( "#article-delete" ).click(function(e) {
+  e.preventDefault();
+  $(".full-article").addClass('de-active');
+});
+
+  })(jQuery);
